796d25b476a1fd65bed69064a0daac8c2b259242
alan <alanitoyah96@gmail.com>
2026-09-17 16:07:25 -0500

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+
+        buy, sell, best = 0, 1, 0
+
+        while sell < len(prices):
+            best = max(prices[sell] - prices[buy], best)
+            if prices[sell] < prices[buy]:
+                buy = sell
+            sell += 1
+
+        return best
+            
